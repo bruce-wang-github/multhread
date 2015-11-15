496cac305d8a78bf95ac1a4e839411261fd22824
U-PCOS-1504171311\Administrator <xiaowenke@zto.cn>
2015-11-15 21:33:16 +0800

    修改测试类

--- src/test/java/top/ddys/multhread/SelectTaskMulThreadServiceTest.java
+++ src/test/java/top/ddys/multhread/SelectTaskMulThreadServiceTest.java
@@ -14,42 +14,10 @@ import java.util.List;
  *          Date Time: a
  *@author: XWK
  */
-public class SelectTaskMulThreadServiceTest extends TestCase {
+public class SelectTaskMulThreadServiceTest extends TestCase implements ISelectTask<String,Boolean>{
 
     public void testExecute() throws Exception {
-        IMulThreadService<String,Boolean> mulThreadService = new SelectTaskMulThreadService(new ISelectTask<String,Boolean>() {
-            /**
-             * Created with IntelliJ IDEA.
-             * 执行任务，返回所有执行的结果
-             * className: TaskMulThreadService
-             *
-             * @author: ddys
-             * @version 1.0
-             * Date Time:
-             */
-            public Boolean execute(String s) {
-                System.out.println(Thread.currentThread().getId()+"线程正在处理"+s);
-                return true;
-            }
-
-            /**
-             * @param 'a 传递参数
-             * @return a 回类型
-             * @throws
-             * @Title: a
-             * @Description: 获取一批任务
-             * @author ddys
-             * @date 2015-11-15 21:09
-             */
-            public String[] getTaskItem() {
-                String [] taskItems = new String[100];
-                for (int i=0;i<100;i++){
-                    taskItems[i]="任务"+i;
-                }
-                return taskItems;
-            }
-        });
-
+        IMulThreadService<String,Boolean> mulThreadService = new SelectTaskMulThreadService(this);
         long start = System.currentTimeMillis();
         List<Boolean> result = mulThreadService.execute();
         for (Boolean e : result){
@@ -58,5 +26,35 @@ public class SelectTaskMulThreadServiceTest extends TestCase {
             }
         }
         System.out.println("所有任务处理完成，耗时"+(System.currentTimeMillis()-start)+",任务成功数"+result.size());
+    }
+    /**
+     * Created with IntelliJ IDEA.
+     * 执行任务，返回所有执行的结果
+     * className: TaskMulThreadService
+     *
+     * @author: ddys
+     * @version 1.0
+     * Date Time:
+     */
+    public Boolean execute(String s) {
+        System.out.println(Thread.currentThread().getId()+"线程正在处理"+s);
+        return true;
+    }
+
+    /**
+     * @param 'a 传递参数
+     * @return a 回类型
+     * @throws
+     * @Title: a
+     * @Description: 获取一批任务
+     * @author ddys
+     * @date 2015-11-15 21:09
+     */
+    public String[] getTaskItem() {
+        String [] taskItems = new String[100];
+        for (int i=0;i<100;i++){
+            taskItems[i]="任务"+i;
+        }
+        return taskItems;
     }
 }
--- src/test/java/top/ddys/multhread/TaskMulThreadServiceTest.java
+++ src/test/java/top/ddys/multhread/TaskMulThreadServiceTest.java
@@ -14,29 +14,14 @@ import java.util.List;
  *          Date Time: a
  *@author: ddys
  */
-public class TaskMulThreadServiceTest extends TestCase {
+public class TaskMulThreadServiceTest extends TestCase implements ITaskHandle<String,Boolean>{
 
     public void testExecute() throws Exception {
         String [] taskItems = new String[100];
         for (int i=0;i<100;i++){
             taskItems[i]="任务"+i;
         }
-        IMulThreadService<String,Boolean> mulThreadService = new TaskMulThreadService(new ITaskHandle<String,Boolean>() {
-
-            /**
-             * Created with IntelliJ IDEA.
-             * 执行任务，返回所有执行的结果
-             * className: TaskMulThreadService
-             *
-             * @author: ddys
-             * @version 1.0
-             * Date Time:
-             */
-            public Boolean execute(String s) {
-                System.out.println(Thread.currentThread().getId()+"线程正在处理"+s);
-                return true;
-            }
-        });
+        IMulThreadService<String,Boolean> mulThreadService = new TaskMulThreadService(this);
         long start = System.currentTimeMillis();
         List<Boolean> result = mulThreadService.execute(taskItems);
         for (Boolean e : result){
@@ -45,5 +30,19 @@ public class TaskMulThreadServiceTest extends TestCase {
             }
         }
         System.out.println("所有任务处理完成，耗时"+(System.currentTimeMillis()-start)+",任务成功数"+result.size());
+    }
+
+    /**
+     * Created with IntelliJ IDEA.
+     * 执行任务，返回所有执行的结果
+     * className: TaskMulThreadService
+     *
+     * @author: ddys
+     * @version 1.0
+     * Date Time:
+     */
+    public Boolean execute(String s) {
+        System.out.println(Thread.currentThread().getId()+"线程正在处理"+s);
+        return true;
     }
 }
